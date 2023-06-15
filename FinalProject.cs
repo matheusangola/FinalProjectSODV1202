29d7637a9be4a0f5d61cdf0950d426c6da570e5c
Matheusangola <matheusangola97@gmail.com>
2023-06-15 09:47:39 -0600

    feat: Added start file

---
+++ FinalProject.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Connect_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Connect 4!");
+        }
+    }
+}
