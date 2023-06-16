5e9ae84234beeb47d0fa250478f09b5b497bbd3c
Matheusangola <matheusangola97@gmail.com>
2023-06-15 21:57:20 -0600

    feat: Created a simple board.

--- FinalProject.cs
+++ FinalProject.cs
@@ -1,12 +1,42 @@
 using System;
+using System.Numerics;
 
 namespace Connect_4
 {
+    public class Board
+    {
+        const int Rows = 6;
+        const int Columns = 7;
+        char[,] board = new char[Rows, Columns];
+
+        public void ViewBoard()
+        {
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Columns; c++)
+                {
+                    Console.Write(board[r, c]);
+                    Console.Write('-');
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        public void NewGame()
+        {
+            ViewBoard();
+        }
+    }
+    
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Connect 4!");
+            Board LoadGame = new Board();
+            LoadGame.NewGame();
+
         }
     }
 }
